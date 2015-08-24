7bebbf17255f54fb35097bc3b71bc549dd2242f6
Gary Siu <Gary@untypeable.com>
2015-08-24 12:35:18 +0100

    Added victory check and additional sanity check

--- js/main.js
+++ js/main.js
@@ -7,9 +7,11 @@ mancala.mancala = [0,0] // mancala.mancala[0] is the far side. We track it seper
 mancala.turn = 1; //Alternates between 0 and 1. We'll start with the player facing toward the screen (near side)
 mancala.hand = 0;
 mancala.direction = 0;
-mancala.oppositeRow;
+mancala.continuePlaying = true;
 mancala.sow = function(row, index) {
-  if(mancala.checkValidSow(row)) {
+  if(mancala.checkValidSow(row, index)) {
+    mancala.checkVictory();
+    if (mancala.continuePlaying === false) { return; }
     mancala.takeSeeds(row,index);
     // Sowing moves to the right. Player 0 is facing towards the screen so their sowing goes relatively to the left. 
     row === 0 ? mancala.direction = -1 : mancala.direction = 1; // Set initial sow direction
@@ -42,7 +44,8 @@ mancala.sow = function(row, index) {
   if(mancala.hand === 0 && mancala.board[row][index] === 1 && row === mancala.turn){ // If you finish your turn on an empty spot on your row
     mancala.board[row][index]--;                                                     // Remove that seed
     mancala.mancala[mancala.turn]++;                                                 // and put it in your mancala
-    row === 0 ? mancala.oppositeRow = 1 : mancala.oppositeRow = 0;
+    var oppositeRow = 0;
+    row === 0 ? oppositeRow = 1 : oppositeRow = 0;
     mancala.mancala[mancala.turn] = mancala.board[mancala.oppositeRow][index];       // Put the seeds in the opposite row in your mancala
     mancala.board[mancala.oppositeRow][index] = 0;                                   // (and empty the pit)
   }
@@ -50,15 +53,31 @@ mancala.sow = function(row, index) {
   console.log('Player turn set to ' + mancala.turn);
   }
 }
-mancala.checkValidSow = function(row) {
+mancala.checkValidSow = function(row, index) {
   if(mancala.turn !== row) {
     console.log('Invalid move. You can only sow from your own row.');
+    return false;
+  } else if(mancala.board[row][index] === 0) {
+    console.log('The pit you selected is empty. Pick another one.');
     return false;
   }
   return true;
 }
-mancala.takeSeeds = function (row, index) {
-    mancala.hand = mancala.board[row][index]; // move the seeds from the board to the hand
-    console.log(mancala.hand +' seeds in the hand');
-    mancala.board[row][index] = 0;
+mancala.takeSeeds = function(row, index) {
+  mancala.hand = mancala.board[row][index]; // move the seeds from the board to the hand
+  console.log(mancala.hand +' seeds in the hand');
+  mancala.board[row][index] = 0;
+}
+mancala.checkVictory = function() {
+  var enemyRow = 0;
+  mancala.turn === 0 ? enemyRow = 1 : enemyRow = 0;
+  if(!mancala.board[enemyRow].reduce(function(a,b){return a + b;})) {                                // If the enemy row is empty
+    mancala.mancala[mancala.turn] = mancala.board[mancala.turn].reduce(function(a,b){return a + b;}) // Add all the remaining seeds on your side
+    for(i = 0; i < mancala.board[mancala.turn].length; i++) {                                        // to your mancala. 
+      mancala.board[mancala.turn][i] = 0; // Empty the board.
+    }
+    console.log('Game over');
+    mancala.mancala[0] > mancala.mancala[1] ? console.log('Player 0 wins') : console.log ('Player 1 wins');
+    continuePlaying = false;
+  }
 }
